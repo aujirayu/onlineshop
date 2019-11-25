2afdd4c060504a1bfbf833c17c714ec7481a4328
Jirayu <jikull55@gmail.com>
2019-11-26 00:02:07 +0700

    เอาไฟล์โปรเจคเข้า

---
+++ OnlineShop/login.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>สารปรับปรุงดินขี้แดดนาเกลือ</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet"
+	href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+	integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+	crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=Rubik:400,700"
+	rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap"
+	rel="stylesheet">
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="css/jquery-ui.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+<link rel="stylesheet" href="css/aos.css">
+
+<link rel="stylesheet" href="css/style.css">
+
+<style>
+@import
+	url('https://fonts.googleapis.com/css?family=Kanit&display=swap&subset=thai')
+	;
+
+body, div {
+	font-family: 'Kanit', sans-serif;
+}
+
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+	width: 100%;
+	padding: 12px 20px;
+	margin: 8px 0;
+	display: inline-block;
+	border: 1px solid #ccc;
+	box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+	background-color: #4CAF50;
+	color: white;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	cursor: pointer;
+	width: 100%;
+}
+
+button:hover {
+	opacity: 0.8;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+	width: auto;
+	padding: 10px 18px;
+	background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+	text-align: center;
+	margin: 24px 0 12px 0;
+	position: relative;
+}
+
+img.avatar {
+	width: 40%;
+	border-radius: 50%;
+}
+
+/* .container { */
+/* 	padding: 16px; */
+/* } */
+
+span.psw {
+	float: right;
+	padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+	display: none; /* Hidden by default */
+	position: fixed; /* Stay in place */
+	z-index: 1; /* Sit on top */
+	left: 0;
+	top: 0;
+	width: 100%; /* Full width */
+	height: 100%; /* Full height */
+	overflow: auto; /* Enable scroll if needed */
+	background-color: rgb(0, 0, 0); /* Fallback color */
+	background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+	padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+	background-color: #fefefe;
+	margin: 5% auto 15% auto;
+	/* 5% from the top, 15% from the bottom and centered */
+	border: 1px solid #888;
+	width: 20%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+	position: absolute;
+	right: 35px;
+	top: 15px;
+	font-size: 40px;
+	font-weight: bold;
+	color: #f1f1f1;
+}
+
+.close:hover, .close:focus {
+	color: #f44336;
+	cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+	-webkit-animation: animatezoom 0.6s;
+	animation: animatezoom 0.6s
+}
+
+@
+-webkit-keyframes animatezoom {
+	from {-webkit-transform: scale(0)
+}
+
+to {
+	-webkit-transform: scale(1)
+}
+
+}
+@
+keyframes animatezoom {
+	from {transform: scale(0)
+}
+
+to {
+	transform: scale(1)
+}
+
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+	span.psw {
+		display: block;
+		float: none;
+	}
+	.cancelbtn {
+		width: 100%;
+	}
+}
+
+.content {
+  max-width: 500px;
+  margin: auto;
+  background: white;
+  padding: 10px;
+}
+</style>
+</head>
+<body>
+
+	<div class="site-wrap">
+
+		<!-- หัวเว็บ -->
+		<div class="site-navbar bg-white py-2">
+			<div class="container">
+				<div class="d-flex align-items-center justify-content-between">
+					<div>
+						<div>
+							<a href="index.php" class="navbar-brand;" style='font-size: 24px'>สารปรับปรุงดินขี้แดดนาเกลือ</a>
+						</div>
+					</div>
+					<div class="main-nav d-none d-lg-block">
+						<nav class="site-navigation text-right text-md-center"
+							role="navigation">
+							<ul class="site-menu js-clone-nav d-none d-lg-block">
+								<li><a href="index.php" style='font-size: 18px'>หน้าหลัก</a></li>
+
+
+								<li><a href="product.php" style='font-size: 18px'>สินค้า</a></li>
+								<li><a href="contact.php" style='font-size: 18px'>ติดต่อ</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="icons">
+						<a href="cart.html" class="icons-btn d-inline-block bag"> <span
+							class="icon-shopping-bag"></span> <!-- จำนวนตามตระกร้าสินค้า --><span class="number">2</span>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<br> <br> <br>
+
+		<div class="bg-light py-3" style='font-size: 12px'>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 mb-0">
+						<a href="index.php">หน้าหลัก</a> <span class="mx-2 mb-0">/</span>
+						<strong class="text-black">เข้าสู่ระบบ</strong>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="site-section">
+			<div class="container">
+				<div align="center">
+					<p>
+						<button
+							onclick="document.getElementById('id01').style.display='block'"
+							style="width: 100px;">Login</button>
+						<br>
+						<button
+							onclick="document.getElementById('id02').style.display='block'"
+							style="width: 100px; background-color: #ff9800;">Sign Up</button>
+					</p>
+				</div>
+
+				<div id="id01" class="modal">
+					<span
+						onclick="document.getElementById('id01').style.display='none'"
+						class="close" title="Close Modal">&times;</span>
+					<form class="modal-content animate" action="/action_page.php"
+						method="post">
+						<div class="imgcontainer"></div>
+
+						<div class="container">
+							<label for="uname"><b>Username</b></label> <input type="text"
+								placeholder="Enter Username" name="uname" required> <label
+								for="psw"><b>Password</b></label> <input type="password"
+								placeholder="Enter Password" name="psw" required>
+
+							<button type="submit">Login</button>
+						</div>
+						<div class="container" style="background-color: #f1f1f1">
+							<button type="button"
+								onclick="document.getElementById('id01').style.display='none'"
+								class="cancelbtn">Cancel</button>
+							<span class="psw">Forgot <a href="#">password?</a></span>
+						</div>
+					</form>
+				</div>
+			</div>
+
+
+
+			<div id="id02" class="modal">
+				<span onclick="document.getElementById('id02').style.display='none'"
+					class="close" title="Close Modal">&times;</span>
+				<form class="modal-content" action="/action_page.php">
+					<div class="container">
+						<h1>Sign Up</h1>
+						<p>Please fill in this form to create an account.</p>
+						<hr>
+						<label for="email"><b>Email</b></label> <input type="text"
+							placeholder="Enter Email" name="email" required> <label for="psw"><b>Password</b></label>
+						<input type="password" placeholder="Enter Password" name="psw"
+							required> <label for="psw-repeat"><b>Repeat Password</b></label>
+						<input type="password" placeholder="Repeat Password"
+							name="psw-repeat" required> <label>
+
+							<div class="clearfix">
+								<button type="button"
+									onclick="document.getElementById('id02').style.display='none'"
+									class="cancelbtn">Cancel</button>
+								<button type="submit" class="signupbtn">Sign Up</button>
+							</div>
+					
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/aos.js"></script>
+
+	<script src="js/main.js"></script>
+
+	<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('id02');
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+ modal.style.display = "none";
+}
+}
+</script>
+</body>
+</html>
