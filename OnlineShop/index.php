d2d78e56676280c6f8cb19327c6ab315a4a6b065
Jirayu <jikull55@gmail.com>
2019-11-25 23:24:42 +0700

    เอาไฟล์โปรเจคเข้า

---
+++ OnlineShop/index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>สารปรับปรุงดินขี้แดดนาเกลือ</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet"
+	href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+	integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+	crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=Rubik:400,700"
+	rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Kanit&display=swap"
+	rel="stylesheet">
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="css/jquery-ui.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+<link rel="stylesheet" href="css/aos.css">
+
+<link rel="stylesheet" href="css/style.css">
+
+<style>
+@import
+	url('https://fonts.googleapis.com/css?family=Kanit&display=swap&subset=thai')
+	;
+
+body, div {
+	font-family: 'Kanit', sans-serif;
+}
+
+#more {
+	display: none;
+}
+
+#more2 {
+	display: none;
+}
+</style>
+</head>
+<body>
+
+	<div class="site-wrap">
+
+		<!-- หัวเว็บ -->
+		<div class="site-navbar bg-white py-2">
+			<div class="container">
+				<div class="d-flex align-items-center justify-content-between">
+					<div>
+						<div>
+							<a href="index.php" class="navbar-brand;" style='font-size: 24px'>สารปรับปรุงดินขี้แดดนาเกลือ</a>
+						</div>
+					</div>
+					<div class="main-nav d-none d-lg-block">
+						<nav class="site-navigation text-right text-md-center"
+							role="navigation">
+							<ul class="site-menu js-clone-nav d-none d-lg-block">
+								<li><a href="index.php" style='font-size: 18px'>หน้าหลัก</a></li>
+
+
+								<li><a href="product.php" style='font-size: 18px'>สินค้า</a></li>
+								<li><a href="contact.php" style='font-size: 18px'>ติดต่อ</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="icons">
+						<a href="cart.html" class="icons-btn d-inline-block bag"> <span
+							class="icon-shopping-bag"></span> <!-- จำนวนตามตระกร้าสินค้า --><span class="number">2</span>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<br> <br> <br>
+
+
+		<!-- ส่วนกลางเว็บ -->
+
+		<div class="site-blocks-cover" data-aos="fade"
+			style="background-color:;">
+			<div class="container">
+				<div class="row align-items-center">
+
+					<div class="col-lg-5 text-center align-self-start">
+						<div class="featured-hero-product w-100">
+							<br> <br> <br>
+							<h1>ปุ๋ยอินทรีย์</h1>
+
+							<h4 align="LEFT">
+								ปุ๋ยอินทรีย์ เป็นปุ๋ยที่มีความสำคัญต่อการปรับปรุงดิน
+								และให้แร่ธาตุอาหารแก่พืช เพราะประกอบด้วยอินทรียวัตถุ
+								และแร่ธาตุต่างๆที่จะทำให้ดินมีสภาพดีขึ้น
+								พร้อมช่วยให้พืชเจริญเติบโตได้อย่างเหมาะสม
+								โดยปุ๋ยอินทรีย์มีความสำคัญ ดังนี้<span id="dots">...</span><span
+									id="more"> <br> 1. ปุ๋ยอินทรีย์มีแร่ธาตุไนโตรเจน ฟอสฟอรัส
+									โพแทสเซียม และธาตุอาหารรอง
+									รวมถึงจุลชีพที่พอเพียงหรือพอเพียงต่อความต้องการของพืช <br> 2.
+									การใช้ปุ๋ยอินทรีย์ในระยะแรกอาจทำให้พืชมีผลผลิตไม่มากนัก
+									แต่หากใช้ในระยะยาว ผลผลิตพืชจะเพิ่มขึ้นมาก
+									เนื่องจากปุ๋ยอินทรีย์ทำให้คุณสมบัติของดินดินดีขึ้นเรื่อยๆ <br>
+									3.
+									ปุ๋ยอินทรีย์ช่วยให้ความเป็นกรด-ด่างของดินมีความเหมาะสมต่อการเติบโตของพืช
+									และช่วยให้ความเป็นกรด-ด่างเปลี่ยนแปลงได้ยากขึ้น
+									รวมถึงช่วยในการดูดยึดธาตุอาหารไว้ในดินได้มากขึ้น <br> 4.
+									ช่วยให้อนุภาคของดินจับตัวกันเป็นก้อนหรือเม็ดดินได้ดี
+									เนื้อดินไม่อัดตัวกันแน่น มีความร่วนซุย การถ่ายเทอากาศ
+									การอุ้มน้ำ และการไหลซึมของน้ำในดินดีขึ้น <br> 5.
+									ช่วยให้จุลินทรีย์ที่มีประโยชน์ในดินสามารถเจริญเติบโตได้ดี
+									เนื่องจากอินทรีย์วัตถุสามารถเอื้อประโยชน์
+									และสร้างสภาวะที่เหมาะสมต่อการเจริญเติบโต <br> 6.
+									ช่วยเพิ่มความสามารถในการย่อยสลายอินทรีย์วัตถุในดินให้ดีขึ้นจากปริมาณจุลินทรีย์ในดินที่เพิ่มขึ้น
+									<br> 7. เป็นปุ๋ยที่สามารถหาได้ง่าย และทำขึ้นได้ง่าย
+									ไม่ยุ่งยากซับซ้อน
+									รวมถึงวัตถุดิบที่ใช้มีราคาถูกสามารถใช้วัตถุดิบทั่วไปตามท้องถิ่น
+									<br> 8. มีวิธีการการใส่ไม่ยุ่งยาก
+									และไม่เป็นอันตรายหรือมีผลข้างเคียงต่อเกษตรกร <br> 9.
+									ธาตุอาหารในปุ๋ยอินทรีย์มีโอกาสสูญเสียจากการซึม การไหลบ่าของน้ำ
+									การเสื่อมสภาพ
+									เนื่องจากสารอาหารหรือแร่ธาตุจะเป็นองค์ประกอบในสารอินทรีย์เหล่านั้น
+								</span>
+							</h4>
+							<p>
+								<button onclick="readMore()"
+									class="btn btn-outline-primary rounded-0" id="myBtn">อ่านต่อ</button>
+							</p>
+						</div>
+					</div>
+					<div>
+						<img src="images/pic-powder.png" alt="Image"
+							class="img-fluid img-1">
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="site-blocks-cover" data-aos="fade"
+			style="background-color:;">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-5 ml-auto order-lg-2 align-self-start">
+						<div class="featured-hero-product w-100">
+							<br> <br>
+							<h1>ขี้แดดนาเกลือ</h1>
+							<h4 align="LEFT">
+								ขี้แดดนาเกลือ หรือ ชาวบ้านเรียกกันว่า หนังหมา คือ
+								สาหร่ายตะไคร่น้ำที่จับตัวกันเป็นแผ่นในนาเกลือ
+								และตกตะกอนแห้งสนิทในนาเกลือ โดยในขี้แดดนาเกลือมีคุณสมบัติทางเคมี
+								มีธาตุอาหารฟอสฟอรัสและโปรแทสเซียม สามารถนำไปใช้เป็นปุ๋ยชีวภาพ
+								คุณสมบัติของขี้แดดนาเกลือคือ<br> 1. ทางกายภาพ
+								รูปร่างภายนอกจะเป็นแผ่นบางๆ หนาประมาณ 2-5 มิลลิเมตร กว้าง-ยาว
+								2-3 นิ้ว มีสีดำอมน้ำตาล เมื่อแห้งสนิท
+								ถ้าใช้มือบีบจะกรอบและหักง่าย เมื่อนำไปแช่น้ำจะอุ้มน้ำได้ดี<br>
+								2. ทางชีวภาพ ขี้แดดนาเกลือ เกิดจากสาหร่าย
+								ตะไคร่น้ำและจุลินทรีย์เล็กๆ ในน้ำจืดและน้ำกร่อย ในนาเกลือ
+								เมื่อถูกแดดจะแห้งเป็นแผ่นบางๆ
+								ถ้านำไปแช่น้ำจะมีกลิ่นเหม็นคล้ายหมาตายเพราะเกิดการเน่า
+								ขี้แดดนาเกลือใหม่ๆ จะมีรสเค็มเล็กน้อย
+								แต่ถ้าเป็นขี้แดดค้างมีรสเค็มจะน้อยลงหรือหายไป<br> 3.
+								คุณสมบัติทางเคมี จากการวิเคราะห์ของภาควิชาเคมี คณะวิทยาศาสตร์
+								มหาวิทยาลัยศิลปากร ดำเนินการวิเคราะห์ โดย ผศ.ดร.ณรงค์ ฉิมพาลี
+								พบว่า ขี้แดดนาเกลือมีธาตุอาหารฟอสฟอรัส (P2O5) 0.13%
+								และโพแทสเซียม (K2O) 2%<br>
+								การใช้ประโยชน์จากขี้แดดนาเกลือในทางการเกษตร เนื่องจากมีธาตุ P2O5
+								และ K2O และช่วยปรับโครงสร้างของดินให้ร่วนซุย จึงมีประโยชน์ดังนี้<span
+									id="dots2">...</span><span id="more2"> <br> 1. ใช้กับไม้ผล<br>
+									เช่นใช้กับชมพู่เพชร ส้มโอขาวใหญ่ มะนาว ละมุด พุทรา ฝรั่ง มะละกอ
+									ทับทิม แตงโม แตงไทย ฯลฯ ปรากฏว่ามีการเจริญเติบโตดี เนื้อแน่น
+									สีสด รสชาติหวานขึ้น<br> 2. ใช้กับพืชผัก<br> ใช้กับบวบ กวางตุ้ง
+									ผักกาดขาว กะเพรา โหระพา ฯลฯ เจริญเติบโตดี ถ้าเป็นพืชหัว
+									หัวจะแน่น ถ้ามีดอกจะดก<br> 3. ใช้เลี้ยงสัตว์<br>
+									ใช้ในการเลี้ยงนกและไก่ จะเจริญเติบโตดี ไก่จะไม่จิกกันเอง
+									ขนไม่หลุดง่าย และสามารถใช้เลี้ยงปลาหมอเทศ และปลากระบอกได้
+								</span>
+							</h4>
+							<p>
+								<button onclick="readMore2()"
+									class="btn btn-outline-primary rounded-0" id="myBtn2">อ่านต่อ</button>
+							</p>
+						</div>
+					</div>
+					<div>
+						<img style="widght: 367px; height: 367px"
+							src="images/201511-02-144330_wJ-0.jpg" alt="Image"
+							class="img-fluid img-8">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- ส่วนท้าย -->
+
+	<footer class="site-footer custom-border-top">
+		<div class="container">
+			<div class="row">
+
+
+				<div class="col-md-4 col-lg-3">
+					<div class="block-5 mb-1">
+						<h3 class="footer-heading mb-4">ติดต่อ</h3>
+						<ul class="list-unstyled">
+							<li class="address">ที่อยู่</li>
+							<li class="phone"><a href="tel://23923929210">เบอร์โทร</a></li>
+							<li class="email">อีเมล</li>
+						</ul>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+	</footer>
+	</div>
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/aos.js"></script>
+
+	<script src="js/main.js"></script>
+
+	<script>
+function readMore() {
+	  var dots = document.getElementById("dots");
+	  var moreText = document.getElementById("more");
+	  var btnText = document.getElementById("myBtn");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "อ่านต่อ"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "ย่อข้อความ"; 
+	    moreText.style.display = "inline";
+	  }
+	}
+	
+function readMore2() {
+	  var dots = document.getElementById("dots2");
+	  var moreText = document.getElementById("more2");
+	  var btnText = document.getElementById("myBtn2");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "อ่านต่อ"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "ย่อข้อความ"; 
+	    moreText.style.display = "inline";
+	  }
+	}
+</script>
+</body>
+</html>
